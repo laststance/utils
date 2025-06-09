e4d565888a812aaf0e15d9c81ffc6c33501cef47
Ryota Murakami <dojce1048@gmail.com>
2025-06-09 21:49:47 +0900

    fix build

--- packages/next-react/components/ui/calendar.tsx
+++ packages/next-react/components/ui/calendar.tsx
@@ -60,12 +60,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ className, orientation, ...props }) => {
+          const ChevronIcon = orientation === "left" ? ChevronLeft : ChevronRight
+          return <ChevronIcon className={cn("size-4", className)} {...props} />
+        },
       }}
       {...props}
     />
