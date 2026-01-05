b18d17b81d9198022394b94c243b05590768dee6
Ryota Murakami <dojce1048@gmail.com>
2026-01-05 20:41:00 +0900

    feat(next-react): add useInitialEffect hook with unit tests
    - Introduced the useInitialEffect hook to execute effects only on the initial render.
    - Added unit tests to verify the correct invocation of the effect function during the initial render and its non-invocation on subsequent renders.
    - Enhanced test coverage for the new hook to ensure expected functionality.

---
+++ packages/next-react/hooks/use-initial-effect.test.ts
@@ -0,0 +1,21 @@
+import { renderHook } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+
+import { useInitialEffect } from './use-initial-efect'
+
+describe('useInitialEffect', () => {
+  it('call effect in initial render', () => {
+    const effect = vi.fn()
+    renderHook(() => useInitialEffect(effect))
+    expect(effect).toHaveBeenCalledOnce()
+  })
+  it('never call effect in secound and subsequent renders', () => {
+    const effect = vi.fn()
+    const { rerender } = renderHook(() => useInitialEffect(effect))
+    expect(effect).toHaveBeenCalledOnce()
+    rerender()
+    expect(effect).toHaveBeenCalledOnce()
+    rerender()
+    expect(effect).toHaveBeenCalledOnce()
+  })
+})
