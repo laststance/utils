59c660f5a748a8788aeae205ee8d4fdefe35c201
Ryota Murakami <dojce1048@gmail.com>
2026-02-24 09:19:11 +0900

    refactor(universal): improve isNullish tests for clarity and coverage
    - Updated test descriptions for better readability, specifying conditions more clearly.
    - Added additional test cases to check for false values, ensuring comprehensive coverage for inputs like false and empty string.

--- packages/universal/isNullish.test.ts
+++ packages/universal/isNullish.test.ts
@@ -3,10 +3,16 @@ import { describe, it, expect } from 'vitest'
 import { isNullish } from './isNullish'
 
 describe('isNullish', () => {
-  it('should return true pass undefined', () => {
+  it('should return true when pass undefined', () => {
     expect(isNullish(undefined)).toBe(true)
   })
-  it('should return true pass null', () => {
+  it('should return true when pass null', () => {
     expect(isNullish(null)).toBe(true)
+  })
+  it('should return false when pass false', () => {
+    expect(isNullish(false)).toBe(false)
+  })
+  it('should return false when pass ""', () => {
+    expect(isNullish('')).toBe(false)
   })
 })
