fcb243817eac8f23dfa740e8ffca1b2f894b5506
Ryota Murakami <dojce1048@gmail.com>
2026-03-17 08:47:47 +0900

    feat(vscode): add configuration to run current TypeScript file with tsx

--- packages/types/zodPlayground/nullable.ts
+++ packages/types/zodPlayground/nullable.ts
@@ -1,1 +1,12 @@
 import { z } from 'zod'
+
+const objSchema = z.object({
+  name: z.string().nullable(),
+  age: z.number().nullable(),
+})
+
+const res = objSchema.safeParse({ name: null, age: null })
+
+if (res.success) {
+  console.log('success')
+}
